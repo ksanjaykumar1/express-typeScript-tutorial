783292926cd2caa7bcdeb46b108475a1dee0be70
Sanjay <ksanjayk@protonmail.com>
2023-05-21 01:03:35 +0530

    dao and dto added

---
+++ users/daos/users.dao.ts
@@ -0,0 +1,17 @@
+import { CreateUserDto } from '../dto/create.user.dto';
+import { PatchUserDto } from '../dto/patch.user.dto';
+import { PutUserDto } from '../dto/put.user.dto';
+import shortid from 'shortid';
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:in-memory-dao');
+
+class UsersDao {
+  users: Array<CreateUserDto> = [];
+
+  constructor() {
+    log('Created new instance of UsersDao');
+  }
+}
+
+export default new UsersDao();
